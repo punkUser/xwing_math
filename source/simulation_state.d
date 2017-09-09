2dc7ee98a24022d95819a89c71633c3d9f34c061
Andrew Lauritzen <andrew.lauritzen@gmail.com>
2017-09-08 20:06:43 -0700

    Precompute probability power table
    Initially when testing this I tested it with attack rolls and it made almost no difference... that's because the majority of forked paths are actually defense rolls since they come lower in the tree...
    
    ~5-10% improvement from this. Likely next bit to look at is the append_state function and related data structure.

--- source/simulation_state.d
+++ source/simulation_state.d
@@ -31,6 +31,30 @@ private pure double factorial(int n)
     return k_factorials_table[n];
 }
 
+private pure double[count] compute_power_table(ulong count)(ulong num, ulong denom)
+{
+    double[15] table;
+    table[0] = 1.0;
+
+    // Can't use pow() as this has to work at compile time, so keep it simple
+    ulong n = num;
+    ulong d = denom;
+    foreach (i; 1 .. table.length)
+    {
+        table[i] = cast(double)n / cast(double)d;
+        n *= num;
+        d *= denom;
+    }
+
+    return table;
+}
+
+private pure double fractional_power(ulong num, ulong denom)(int power)
+{
+    static immutable auto k_power_table = compute_power_table!15(num, denom);
+    assert(power < k_power_table.length);
+    return k_power_table[power];
+}
 
 // Multinomial distribution: https://en.wikipedia.org/wiki/Multinomial_distribution
 // roll_probability = n! / (x_1! * ... * x_k!) * p_1^x_1 * ... p_k^x_k
@@ -45,7 +69,9 @@ private pure double compute_attack_roll_probability(int blank, int focus, int hi
     // P(crit)  = 1/8
     double nf = factorial(blank + focus + hit + crit);
     double xf = (factorial(blank) * factorial(focus) * factorial(hit) * factorial(crit));
-    double p = pow(0.25, blank + focus) * pow(0.375, hit) * pow(0.125, crit);
+
+    //double p = pow(0.25, blank + focus) * pow(0.375, hit) * pow(0.125, crit);
+    double p = fractional_power!(1, 8)(crit) * fractional_power!(2, 8)(blank + focus) * fractional_power!(3, 8)(hit);
 
     double roll_probability = (nf / xf) * p;
 
@@ -60,7 +86,9 @@ private pure double compute_defense_roll_probability(int blank, int focus, int e
     // P(evade) = 3/8
     double nf = factorial(blank + focus + evade);
     double xf = (factorial(blank) * factorial(focus) * factorial(evade));
-    double p = pow(0.375, blank + evade) * pow(0.25, focus);
+
+    //double p = pow(0.375, blank + evade) * pow(0.25, focus);
+    double p = fractional_power!(2, 8)(focus) * fractional_power!(3, 8)(blank + evade);
 
     double roll_probability = (nf / xf) * p;
 
