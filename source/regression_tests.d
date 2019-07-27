d608dd45919fc19b80d886a525c07a1ff614225c
Andrew Lauritzen <andrew.lauritzen@gmail.com>
2019-07-27 10:18:11 -0700

    Fix typo and add Finn+Heroic test

--- source/regression_tests.d
+++ source/regression_tests.d
@@ -26,28 +26,29 @@ private immutable double k_expected_damage_epsilon = 5e-16;
 // Test name abbreviations:
 //   f = focus, l = lock, c = calculate, o = force, e = evade
 private static immutable Test[] k_regression_test_cases = [
-    { "3f vs 3f",               "d=gwAAAAAAAAA&a1=MQgAAAAAAAA",                     0.637756347656250 },
-    { "3lo+LW vs 0",            "d=gAAAAAAAAAA&a1=MwEAAAAIAAA",                     2.695068359375000 }, // Tests attacker forward search
-    { "3fl vs 3fe+LW",          "d=gyAABAAAAAA&a1=MwgAAAAAAAA",                     0.144477188587189 },
-    { "2x 3fl vs 3fe+re",       "d=gyABAAAAAAA&a1=MwgAAAAAAAA&a2=MwgAAAAAAAA",      0.937748962769547 },
-    { "2x 3fl vs 3fe",          "d=gyAAAAAAAAA&a1=MwgAAAAAAAA&a2=MwgAAAAAAAA",      1.429703696999240 },
-    { "3 vs 0 biggs",           "d=AAAAEAAAAAA&a1=MQAAAAAAAAA",                     0.625             },
-    { "3l fearless vs 0",       "d=AAAAAAAAAAA&a1=MwAAAAAAAgA",                     2.828125000000000 },
-    { "4 all hits",             "d=AAAAAAAAAAA&a1=QQAAAAACAAA",                     4.0               },
-    { "0+shara fl",             "d=AAAAAAAAAAA&a1=AwgAAAIAAAA",                     1.0               }, // Shara add result even without dice
-    { "3fl vs 1fl+shara",       "d=gQAAAAQBAAA&a1=MwgAAAAAAAA",                     1.194763183593750 },
-    { "6x 2f+howl vs 3fe+Iden", "d=gyAAQAAAAAA&a1=IQgAAAAEAAA&a2=IQgAAAAEAAA&a3=IQgAAAAEAAA&a4=IQgAAAAEAAA&a5=IQgAAAAEAAA&a6=IQgAAAAEAAA",
-                                                                                    1.740283006218120 },
-    { "4 vs 0+L337",            "d=AAAAAEAAAAA&a1=QQAAAAAAAAA",                     1.625             },
-    { "3l vs 0+L337",           "d=AAAAAEAAAAA&a1=MwAAAAAAAAA",                     1.5               }, // Always use L3-37 except on all blanks (better than attacker rerolling just blanks/focus)
-    { "3f vs 2o+ezra",          "d=EgAIAAwAAAA&a1=MQgAAAAAAAA",                     1.074462890625000 },
-    { "3fl vs 2e+rebel_falcon", "d=AiAAAAAACAA&a1=MwgAAAAAAAA",                     1.065373420715332 },
-    { "3l+finn vs 0",           "d=AAAAAAAAAAA&a1=MwAAAAAAAAg",                     2.75              },
-    { "3fl vs 2f+heroic",       "d=ggAAAAAAIAA&a1=MwgAAAAAAAA",                     1.391961872577667 },
-    { "3fl+Han vs 0",           "d=AAAAAAAAAAAA&a1=MwgAAAsAAAA",                    2.891601562500000 },
-    { "3f vs 1+LW+Han",         "d=AQAABA4AAAAA&a1=MQgAAAAAAAA",                    1.505950927734375 },
-    { "3f+Zuckuss vs 3f",       "d=gwAAAAAAAAAA&a1=MQgAAAAAAAAB",                   0.912757873535156 },
-    { "2f+Heroic vs 2+Heroic",  "d=AgAAAAAAIAAA&a1=IQgAAAAAACAA",                   0.805532455444336 },
+    { "3f vs 3f",                  "d=gwAAAAAAAAA&a1=MQgAAAAAAAA",                     0.637756347656250 },
+    { "3lo+LW vs 0",               "d=gAAAAAAAAAA&a1=MwEAAAAIAAA",                     2.695068359375000 }, // Tests attacker forward search
+    { "3fl vs 3fe+LW",             "d=gyAABAAAAAA&a1=MwgAAAAAAAA",                     0.144477188587189 },
+    { "2x 3fl vs 3fe+re",          "d=gyABAAAAAAA&a1=MwgAAAAAAAA&a2=MwgAAAAAAAA",      0.937748962769547 },
+    { "2x 3fl vs 3fe",             "d=gyAAAAAAAAA&a1=MwgAAAAAAAA&a2=MwgAAAAAAAA",      1.429703696999240 },
+    { "3 vs 0 biggs",              "d=AAAAEAAAAAA&a1=MQAAAAAAAAA",                     0.625             },
+    { "3l fearless vs 0",          "d=AAAAAAAAAAA&a1=MwAAAAAAAgA",                     2.828125000000000 },
+    { "4 all hits",                "d=AAAAAAAAAAA&a1=QQAAAAACAAA",                     4.0               },
+    { "0+shara fl",                "d=AAAAAAAAAAA&a1=AwgAAAIAAAA",                     1.0               }, // Shara add result even without dice
+    { "3fl vs 1fl+shara",          "d=gQAAAAQBAAA&a1=MwgAAAAAAAA",                     1.194763183593750 },
+    { "6x 2f+howl vs 3fe+Iden",    "d=gyAAQAAAAAA&a1=IQgAAAAEAAA&a2=IQgAAAAEAAA&a3=IQgAAAAEAAA&a4=IQgAAAAEAAA&a5=IQgAAAAEAAA&a6=IQgAAAAEAAA",
+                                                                                       1.740283006218120 },
+    { "4 vs 0+L337",               "d=AAAAAEAAAAA&a1=QQAAAAAAAAA",                     1.625             },
+    { "3l vs 0+L337",              "d=AAAAAEAAAAA&a1=MwAAAAAAAAA",                     1.5               }, // Always use L3-37 except on all blanks (better than attacker rerolling just blanks/focus)
+    { "3f vs 2o+ezra",             "d=EgAIAAwAAAA&a1=MQgAAAAAAAA",                     1.074462890625000 },
+    { "3fl vs 2e+rebel_falcon",    "d=AiAAAAAACAA&a1=MwgAAAAAAAA",                     1.065373420715332 },
+    { "3l+finn vs 0",              "d=AAAAAAAAAAA&a1=MwAAAAAAAAg",                     2.75              },
+    { "3fl vs 2f+heroic",          "d=ggAAAAAAIAA&a1=MwgAAAAAAAA",                     1.391961872577667 },
+    { "3fl+Han vs 0",              "d=AAAAAAAAAAAA&a1=MwgAAAsAAAA",                    2.891601562500000 },
+    { "3f vs 1+LW+Han",            "d=AQAABA4AAAAA&a1=MQgAAAAAAAA",                    1.505950927734375 },
+    { "3f+Zuckuss vs 3f",          "d=gwAAAAAAAAAA&a1=MQgAAAAAAAAB",                   0.912757873535156 },
+    { "2f+Heroic vs 2+Heroic",     "d=AgAAAAAAIAAA&a1=IQgAAAAAACAA",                   0.805532455444336 },
+    { "3 hits vs 2f+Finn+Heroic",  "d=ggAAAAoAIAAA&a1=MQAAAAACAAAA",                   0.574218750000000 },
 ];
 
 // State set is cleared - merely passed in to reuse memory
